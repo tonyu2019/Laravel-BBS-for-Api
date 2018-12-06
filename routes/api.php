fc04aca4bab735152d0afb024b6998b347a0d6c4
646547989 <646547989@qq.com>
2018-12-06 14:03:39 +0800

    节流，防止短信轰炸

--- routes/api.php
+++ routes/api.php
@@ -23,9 +23,17 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-    // 用户注册
-    $api->post('users', 'UserController@store')->name('api.users.store');
+    //防止短信轰炸，1分钟限制一次获取验证码
+    $api->group([
+        'middleware'    => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.sign.expires'),
+    ], function ($api){
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UserController@store')->name('api.users.store');
+    });
+
 });
