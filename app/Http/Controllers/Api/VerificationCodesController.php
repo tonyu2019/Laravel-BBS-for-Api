dae99869f185fc144b04717c3019e5920397ab66
646547989 <646547989@qq.com>
2018-12-06 13:30:11 +0800

    发送验证码

---
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Requests\Api\VerificationCodeRequest;
+use Overtrue\EasySms\EasySms;
+use Overtrue\EasySms\Exceptions\NoGatewayAvailableException;
+
+class VerificationCodesController extends BaseController
+{
+    public function store(VerificationCodeRequest $request, EasySms $easySms){
+        $phone = $request->phone;
+
+        if (!app()->environment('production')) {
+            $code = '9999';
+        }else{
+            // 生成4位随机数，左侧补0
+            $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
+
+            try {
+                $result = $easySms->send($phone, [
+                    'content'  =>  "【于海洋test】您的验证码是{$code}"
+                ]);
+            } catch (\Overtrue\EasySms\Exceptions\NoGatewayAvailableException $exception) {
+                $message = $exception->getException('yunpian')->getMessage();
+                return $this->response->errorInternal($message ?: '短信发送异常');
+            }
+        }
+
+        $key = $phone.'_'.uniqid('tony');
+        //过期时间
+        $expiredAt = now()->addMinutes(10);
+        //缓存验证码，10分钟后过期
+        \Cache::put($key, ['phone' => $phone, 'code' => $code], $expiredAt);
+
+        return $this->response->array([
+            'key' => $key,
+            'expired_at'    => $expiredAt->toDateTimeString(),
+        ])->setStatusCode(201);
+    }
+}
